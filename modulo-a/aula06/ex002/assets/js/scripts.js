33c2fcc0b7f24e659c5bf271419f66877fc7df5b
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2022-03-26 08:18:14 -0300

    commit

---
+++ modulo-a/aula06/ex002/assets/js/scripts.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual seu nome?')
+// VAI PERGUNTAR O NOME
+window.alert('É um prazer te conhecer, '+ nome + '!') 
+// CONCATENAÇÃO
---
+++ modulo-a/aula06/ex003/assets/js/scripts.js
@@ -0,0 +1,11 @@
+// var n1 = Number.parseFloat(window.prompt('Digite um Numero: '))
+// var n2 = Number.parseFloat(window.prompt('Digite outro Numero: '))
+
+var n1 = Number(window.prompt('Digite um Numero: '))
+var n2 = Number(window.prompt('Digite outro Numero: '))
+
+var s = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}`)
+
+// (number + number para adição)
+// (string + string para concatenação)
